465a6cc687865c1df4385998bd70eacdadd2961a
Nick Williams <nick.williams@striderhq.local>
2018-10-04 15:36:40 -0600

    dadadaa

--- src/views/RaceByGroupView.java
+++ src/views/RaceByGroupView.java
@@ -9,11 +9,17 @@ import javax.swing.table.JTableHeader;
 
 
 public class RaceByGroupView {
-
+	
+	
+	//property declarations
+	
 	private JFrame frame;
 	private JTable table;
 	public AgeGroup raceGroup;
-
+	public Object[] col_names;
+	public Object[][] row_values;
+	
+	
 	/**
 	 * overloading the only constructor method that
 	 * will be used for the time being, passing in 
@@ -44,10 +50,18 @@ public class RaceByGroupView {
 		frame.getContentPane().add(table);
 	}
 	private void initialize_with_ag() {
+		/**
+		 * here we will set our frame, create our Object[] 
+		 * for column headers and collect our row values into a 
+		 * Object[][], after this we can create the table and set the frame
+		 * to visible -> true
+		 */
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1970, 799);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		/*
 		for(int race_idx = 0; race_idx < this.raceGroup.get_heats().size(); race_idx++) {
 			Race loop_race = this.raceGroup.get_heats().get(race_idx);
 			Object[] col_names = new String[loop_race.get_num_racers()];
@@ -58,6 +72,15 @@ public class RaceByGroupView {
 				JTable table = new JTable(row_vals, col_names);
 				frame.getContentPane().add(table);
 			}
+		}
+		*/
+		this.col_names = new Object[this.raceGroup.get_heats().get(0).get_num_racers() + 1];
+		this.col_names[0] = "Race Name/ID";
+		for(int x = 0; x < this.raceGroup.get_heats().get(0).get_num_racers() - 1; x++) {
+			this.col_names[x+1] = String.format("%d pos", x+1);
+		}
+		for(int ix = 0; ix < this.raceGroup.get_heats().size(); ix++) {
+			
 		}
 	}
 
